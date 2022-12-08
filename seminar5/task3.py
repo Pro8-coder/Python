606d00fa0624974e9bd513f07233b4ed5793cb63
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-12-09 02:43:42 +0300

    seminar5_task3

--- seminar5/task3.py
+++ seminar5/task3.py
@@ -1,6 +1,3 @@
-from my_functions import open_r_utf
-
-
 # 3) Создать текстовый файл (не программно), построчно записать фамилии
 # сотрудников и величину их окладов (не менее 10 строк). Определить, кто из
 # сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
@@ -8,6 +5,15 @@ from my_functions import open_r_utf
 # Пример файла:
 # Иванов 23543.12
 # Петров 13749.32
+
+
+def open_r_utf(file_name):
+    try:
+        with open(file_name, encoding='utf=8') as f_obj:
+            lst = [{line.split()[0]: float(line.split()[1])} for line in f_obj]
+    except IOError:
+        print('Произошла ошибка ввода/вывода')
+    return lst
 
 
 def task3():
