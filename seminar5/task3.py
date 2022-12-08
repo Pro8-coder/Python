a31cce1d38260d4d305a639b68c18dc66348ae4f
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-12-09 02:48:00 +0300

    seminar5_task3

--- seminar5/task3.py
+++ seminar5/task3.py
@@ -7,9 +7,9 @@
 # Петров 13749.32
 
 
-def open_r_utf(file_name):
+def open_r_utf():
     try:
-        with open(file_name, encoding='utf=8') as f_obj:
+        with open('task3.txt', encoding='utf=8') as f_obj:
             lst = [{line.split()[0]: float(line.split()[1])} for line in f_obj]
     except IOError:
         print('Произошла ошибка ввода/вывода')
@@ -18,11 +18,11 @@ def open_r_utf(file_name):
 
 def task3():
     average_salary = 0
-    for i in open_r_utf('task3.txt'):
+    for i in open_r_utf():
         average_salary += i.get(*i.keys())
         if i.get(*i.keys()) < 20000:
             print(*i.keys())
-    return average_salary / len(open_r_utf('task3.txt'))
+    return average_salary / len(open_r_utf())
 
 
 print(f'Средняя величина дохода сотрудников: {round((task3()), 2)}')
