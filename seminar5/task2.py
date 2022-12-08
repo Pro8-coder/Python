0093916f92a0f9b828d2134fdc5165759547d478
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-12-09 01:01:10 +0300

    seminar5_task2

--- seminar5/task2.py
+++ seminar5/task2.py
@@ -1,13 +1,13 @@
 # 2) Создать текстовый файл (не программно), сохранить в нем несколько строк,
 # выполнить подсчет количества строк, количества слов в каждой строке.
 
-with open('task2.txt', encoding='utf=8') as task2:
-    lines = 0
-    try:
+lines = 0
+try:
+    with open('task2.txt', encoding='utf=8') as task2:
         for i in task2:
             words = len(i.split())
             print(f'В строке "{i}": {words} слов')
             lines += 1
         print(f'Всего {lines} строк')
-    except IOError:
-        print('Произошла ошибка ввода/вывода')
+except IOError:
+    print('Произошла ошибка ввода/вывода')
