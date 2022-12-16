bc1aed8a323d87d2dc306c7c73ec59c927b9fa37
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-12-16 12:29:11 +0300

    seminar6_task1

--- seminar6/task1.py
+++ seminar6/task1.py
@@ -9,12 +9,12 @@ print(f'Старый: ', timeit("season_rus(month)",
                           setup="from seminar2_task3 import season_rus, month",
                           number=10000000))
 print(f'Новый: ', timeit("new_season_rus(month)",
-                          setup="from seminar2_task3 import new_season_rus, month",
-                          number=10000000))
+                         setup="from seminar2_task3 import new_season_rus, month",
+                         number=10000000))
 
 '''
 seminar2_task2.py
-Новый код работает быстрее за счёт множественного присвоения 
+Новый код работает быстрее за счёт множественного присвоения
 и отказа от временной переменной.
 '''
 print('seminar2_task2')
@@ -22,8 +22,8 @@ print(f'Старый: ', timeit("exchange_neighbor_elements(lst)",
                           setup="from seminar2_task2 import exchange_neighbor_elements, lst",
                           number=10000000))
 print(f'Новый: ', timeit("new_exchange_neighbor_elements(lst)",
-                          setup="from seminar2_task2 import new_exchange_neighbor_elements, lst",
-                          number=10000000))
+                         setup="from seminar2_task2 import new_exchange_neighbor_elements, lst",
+                         number=10000000))
 
 '''
 seminar4_task1.py
@@ -34,5 +34,5 @@ print(f'Старый: ', timeit("payroll_preparation(lst)",
                           setup="from seminar4_task1 import payroll_preparation, lst",
                           number=10000000))
 print(f'Новый: ', timeit("new_payroll_preparation(lst)",
-                          setup="from seminar4_task1 import new_payroll_preparation, lst",
-                          number=10000000))
+                         setup="from seminar4_task1 import new_payroll_preparation, lst",
+                         number=10000000))
